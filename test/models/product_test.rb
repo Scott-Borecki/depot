5e64e61f6b5edff47b8b39c7f6aca518ef1316ca
Scott Borecki <scottborecki@gmail.com>
2022-01-04 07:08:06 -0700

    Add store#index

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -64,4 +64,26 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal [I18n.translate('errors.messages.taken')],
                  product.errors[:title]
   end
+
+  test 'product title is at least 10 characters' do
+    product_with_short_title = Product.new(
+      title:       '2short',
+      description: 'yyy',
+      price:       1,
+      image_url:  'fred.gif')
+
+    assert product_with_short_title.invalid?,
+      "#{product_with_short_title.title} shouldn't be valid"
+      assert_equal ['must be at least 10 characters'],
+        product_with_short_title.errors[:title]
+
+    product_with_long_title = Product.new(
+      title:       'This is Long Enough',
+      description: 'yyy',
+      price:       1,
+      image_url:  'fred.gif')
+
+    assert product_with_long_title.valid?,
+      "#{product_with_long_title.title} should be valid"
+  end
 end
